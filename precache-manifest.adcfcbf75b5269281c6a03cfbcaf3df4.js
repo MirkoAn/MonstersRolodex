2067309cf47954903adb80dceff373a7ffc57ceb
Mirko <mirko2792@gmail.com>
2020-10-19 11:12:18 -0500

    Updates

---
+++ precache-manifest.adcfcbf75b5269281c6a03cfbcaf3df4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f302e7770dbe036740eeaef42c7713ec",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "d2d185e03bb8fc0d268e",
+    "url": "/MonstersRolodex/static/css/main.5ad58274.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/MonstersRolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MonstersRolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d185e03bb8fc0d268e",
+    "url": "/MonstersRolodex/static/js/main.ab2b0ca6.chunk.js"
+  },
+  {
+    "revision": "425f1fd85b5840b582ed",
+    "url": "/MonstersRolodex/static/js/runtime-main.2b75c539.js"
+  }
+]);
